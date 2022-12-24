358077d6419a95a4bc363b0f9f0738eb15cb47fc
SparshChaurasia <sparshchaurasia0021@gmail.com>
2022-12-24 16:01:39 +0530

    moved dashboard to a seprate route

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, "index.html")
-
-def dashboard(request):
-    return render(request, "dashboard.html")
