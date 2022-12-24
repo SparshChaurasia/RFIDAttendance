c6eba218ce2d87480c5376bee5bc4a0eabce9e9c
SparshChaurasia <sparshchaurasia0021@gmail.com>
2022-12-24 11:51:31 +0530

    created models for api route

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Classes(models.TextChoices):
+    _9A = "9A"
+    _9B = "9B"
+    _10A = "10A"
+    _10B = "10B"
+
+class Student(models.Model):
+    UID = models.CharField(max_length=12, primary_key=True)
+    Name = models.CharField(max_length=255)
+    Class = models.CharField(max_length=3, choices=Classes.choices)
+
+    def save(self, *args, **kwargs):
+        self.Name = self.Name.lower()
+        self.UID = self.UID.upper()
+        return super(Student, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.Name
+
+class Entry(models.Model):
+    Stud = models.ForeignKey(Student, models.CASCADE)
+    DateTime = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        return super(Entry, self).save(*args, **kwargs)
+
+    def __str__(self):
+        _n = self.Stud.Name
+        _t = self.DateTime.strftime("%m/%d/%Y, %H:%M:%S")
+        return f"{_n} {_t}"
