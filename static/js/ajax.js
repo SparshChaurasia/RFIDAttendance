4fcf5d40b44790171541968f2a82038dc3859c52
SparshChaurasia <sparshchaurasia0021@gmail.com>
2022-12-27 20:56:55 +0530

    created basic updation system

---
+++ static/js/ajax.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    setInterval(function(){
+        $.ajax({
+            type: "POST",
+            url: "http://127.0.0.1:8000/api/list",
+            success: function(response){
+                $("#table-data-container").empty()
+                var i = 1;
+                for (var key in response) {
+                    var entry = response[key];
+                    var html = `<tr>
+                                    <th scope="row">${i}</th>
+                                    <td>${entry.StudName}</td>
+                                    <td>${entry.StudClass}</td>
+                                    <td>${entry.DateTime}</td>
+                                    <td>kharab hai</td>
+                                </tr>`;
+                    $("#table-data-container").append(html);
+
+                    i++;
+                }
+            },
+            error: function(response){
+                alert("An error occured")
+            }
+        });
+    }, 1500);
+});
