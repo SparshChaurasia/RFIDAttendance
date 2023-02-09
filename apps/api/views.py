d3115910115e50f7664f8856a851ab96631afdf9
SparshChaurasia <sparshchaurasia0021@gmail.com>
2023-02-09 23:23:51 +0530

    displayed date on the dashboard; added date filter

--- apps/api/views.py
+++ apps/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
+from datetime import date
 from .models import Student, Entry
 
 @csrf_exempt
@@ -28,6 +29,6 @@ def r_list(request):
     if request.method != "POST":
         return HttpResponse("Invalid request method!") 
 
-    entry = Entry.objects.all()
+    entry = Entry.objects.all().filter(Date=date.today())
     res = JsonResponse(list(entry.values()), safe=False)
     return res
