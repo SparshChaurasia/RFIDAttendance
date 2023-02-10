5bc5c8114348d78b4708cc0c530fee1f905c2966
SparshChaurasia <sparshchaurasia0021@gmail.com>
2023-02-10 19:39:15 +0530

    modified port number

--- static/arduino/connection.py
+++ static/arduino/connection.py
@@ -1,7 +1,7 @@
 import serial
 import requests
 
-PORT = "com6"
+PORT = "com5"
 BAUDRATE = 9600
 
 conn = serial.Serial(PORT, BAUDRATE)
@@ -13,5 +13,9 @@ while True:
     body = {"uid": uid}
     res = requests.post("http://127.0.0.1:8000/api/new", data=body)
     print("Response:", res.text)
+
+    # with open("static\error.html", "w") as f:
+    #     print(res.text, file=f)
+
     print("---------------------")
 conn.close()
